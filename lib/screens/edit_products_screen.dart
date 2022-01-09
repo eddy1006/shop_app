5df08df202a93aa70301ba804bc3755d80944304
Eddy <adigupta239@gmail.com>
2022-01-09 13:48:25 +0530

    added the focus node

--- lib/screens/edit_products_screen.dart
+++ lib/screens/edit_products_screen.dart
@@ -7,6 +7,8 @@ class EditProductsScreen extends StatefulWidget {
 }
 
 class _EditProductsScreenState extends State<EditProductsScreen> {
+  final _priceFocusNode = FocusNode();
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -23,6 +25,16 @@ class _EditProductsScreenState extends State<EditProductsScreen> {
                   label: Text('Title'),
                 ),
                 textInputAction: TextInputAction.next,
+                onFieldSubmitted: (_) =>
+                    FocusScope.of(context).requestFocus(_priceFocusNode),
+              ),
+              TextFormField(
+                decoration: const InputDecoration(
+                  label: Text('Price'),
+                ),
+                textInputAction: TextInputAction.next,
+                keyboardType: TextInputType.number,
+                focusNode: _priceFocusNode,
               ),
             ],
           ),
